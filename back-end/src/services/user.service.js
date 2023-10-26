e7714aae73c7bd49ef1dc33b3190ce882d4d549c
Laysa Bernardes <laysabernardes.ads@gmail.com>
2023-10-26 12:53:09 -0300

    Creation of services

---
+++ back-end/src/services/user.service.js
@@ -0,0 +1,12 @@
+import User from "../models/models.js";
+//.create, .find, .findById , .findOneAndUpdate são metodos do mongoose. 
+
+const createService = (body) => User.create(body);// Função para criar um novo usuário no bd
+const findAllService = () => User.find();// Função para encontrar todos os usuários no bd
+const findByIdService = (id) => User.findById(id);// Função para encontrar um usuário pelo seu ID no bd
+
+// Função para atualizar as informações de um usuário no banco de dados com base no ID.
+const updateService = (id,name,email,password,avatar) => User.findOneAndUpdate({_id: id},{name,email,password,avatar});
+// { _id: id } é o critério de consulta para encontrar o usuário pelo ID. E o restante são valores a serem atualizados.
+
+export default {createService, findAllService, findByIdService, updateService};
